8d5e091459c32b46779f5ef2b04bc057f4307f36
Antama10 <161932080+Antama10@users.noreply.github.com>
2025-09-22 21:51:37 +0200

    Update raetsel.js

--- pages/raetsel.js
+++ pages/raetsel.js
@@ -3,17 +3,22 @@ import React from 'react'
 function Raetsel() {
     return (
         <div className='text-center'>
-            <h4>Nun hier steht dein letztes Rätsel bevor meine Prinzessin!</h4>
+            <h4>Nun hier steht dir dein letztes Rätsel bevor meine Prinzessin!</h4>
             <p className='mt-2'>
-                Als Prinzessin hast du dich heute schon sehr bewiesen, du hast deine geografischen Fähigkeiten gezeigt,
+                Als Prinzessin hast du dich heute schon sehr bewiesen, du hast deine geografischen Fähigkeiten unter Beweis gestellt,
                 kryptisch formulierte Fragen entschlüsselt den Weg hier in die weiten des Internets gefunden, nach einen kleinen
-                Spaziergang das nächste Essen in Angriff genommen und danach auch noch schlau kombiniert eine richtige Kombonation halt ;)
+                Spaziergang das nächste Essen in Angriff genommen, und danach auch noch schlau kombiniert eine richtige Kombonation halt ;)
                 Nun darf ich dich meine kleine Geburtstags Prinzessin noch ein letztes mal herausfordern und hoffe du hast viel Freude bei
-                der Auflösung :)
+                der Auflösung :) <br /> <br /> Ziel ist eine Adresse:
+                <br /> <b>Ort:</b> Die Bauern die tuns täglich, je nach modernität einfach oder kläglich, egal ob bei Kuh, oder Ziege nur das Männchen kriegt die Biege!
+                <br /> <b>Adresstext 1:</b> Wenn eine Kugel nicht steht dann ..... sie plus fahren nur mit ä statt dem a !
+                <br /> <b>Adresstext 2</b> Und wenn man nicht grade im Dreck fährt dann auf der .....!
+                <br /> <br />
+                <b>Setze die Antworten zusammen und dann fahr ma scho los :P</b>
             </p>
         </div>
     )
 }
 
-
 export default Raetsel
+
